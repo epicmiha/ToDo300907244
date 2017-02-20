48acf20a5e9ea81e9e03ac5877602af0c9f8a8c5
serhiipianykh <serhiipianykh@gmail.com>
2017-02-20 18:25:30 -0500

    UI and model

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, ’10.2’
+
+target 'ToDo300907244' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ToDo300907244
+
+pod 'Firebase/Core'
+pod 'Firebase/Database'
+pod 'Firebase/Auth'
+
+end
---
+++ ToDo300907244/ToDo.swift
@@ -0,0 +1,76 @@
+//
+//  ToDo.swift
+//  ToDo300907244
+//
+//  Created by Serhii Pianykh on 2017-02-20.
+//  Copyright © 2017 Mykhailo Obelchak. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class ToDo {
+    private var _name: String!
+    private var _details: String?
+    private var _done: Bool!
+    private var _ref: FIRDatabaseReference?
+    
+    var name: String{
+        get {
+            return _name
+        }
+        set {
+            _name = newValue
+        }
+    }
+    
+    var details: String? {
+        get {
+            return _details
+        }
+        set {
+            _details = newValue
+        }
+    }
+    
+    var done: Bool {
+        get {
+            return _done
+        }
+        set {
+            _done = newValue
+        }
+    }
+    
+    var ref: FIRDatabaseReference? {
+        get {
+            return _ref
+        }
+        set {
+            _ref = newValue
+        }
+    }
+    
+    init(name: String) {
+        self._name = name
+        self._done = false
+    }
+    
+    init(snapshot: FIRDataSnapshot) {
+        let snapshotValue = snapshot.value as! [String: AnyObject]
+        self._name = snapshotValue["name"] as! String
+        self._details = (snapshotValue["details"] as! String)
+        self._done = snapshotValue["done"] as! Bool
+        _ref = snapshot.ref
+    }
+    
+    func toAnyObject() -> Any {
+        return [
+            "name": name,
+            "details": details ?? "",
+            "done": done
+        ]
+    }
+
+    
+}
